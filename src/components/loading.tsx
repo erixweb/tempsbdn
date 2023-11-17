9615980e17b6fd3efe5f0408c36af1231867a8c2
erixweb <erikmorenodev@gmail.com>
2023-11-17 14:53:55 +0100

    Add loading

---
+++ src/components/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading () {
+    return (
+        <main class="w-full max-w-[50em] p-[16px] m-auto h-[100vh] flex justify-center items-center">
+            <h1 class="font-bold text-[30px]">
+                Carregant
+            </h1>
+        </main>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,14 @@ import { lazy } from "preact/compat"
 import { Suspense } from "preact/compat"
 
 import NotFound from "./404"
+import Loading from "./components/loading"
 const App = lazy(() => import("./App"))
 const ECMWFPrediction = lazy(() => import("./prediccio/ecmwf"))
 const Graphics = lazy(() => import("./grafics/grafics"))
 const PrediccioPage = lazy(() => import("./prediccio/prediccio"))
 
 render(
-	<Suspense fallback={"Carregant..."}>
+	<Suspense fallback={<Loading/>}>
 		<Router>
 			<App path="/" />
 			<PrediccioPage path="/prediccio" />
